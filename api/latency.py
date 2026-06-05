344d753c350080fffa59a2f581d5b3cabbf2f2c4
Vineet Kumar <24f1001327@ds.study.iitm.ac.in>
2026-06-06 03:44:31 +0530

    Update latency.py

--- api/latency.py
+++ api/latency.py
@@ -7,6 +7,14 @@ import numpy as np
 
 app = FastAPI()
 
+@app.middleware("http")
+async def add_cors_headers(request: Request, call_next):
+    response = await call_next(request)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "POST, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "*"
+    return response
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
